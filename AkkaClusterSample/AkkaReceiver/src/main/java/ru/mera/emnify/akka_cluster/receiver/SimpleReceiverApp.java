7334916de3445c0d3810dca45840ec7189063d85
Vladimir Boronin <vboronin@mera.ru>
2014-11-26 12:39:22 +0300

    added integration test inside of AkkaClusterTest module

--- AkkaClusterSample/AkkaReceiver/src/main/java/ru/mera/emnify/akka_cluster/receiver/SimpleReceiverApp.java
+++ AkkaClusterSample/AkkaReceiver/src/main/java/ru/mera/emnify/akka_cluster/receiver/SimpleReceiverApp.java
@@ -2,10 +2,13 @@ package ru.mera.emnify.akka_cluster.receiver;
 
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 public class SimpleReceiverApp {
 	public static void main(String[] args) {
-	      ActorSystem system = ActorSystem.create("ClusterSystem");
+        Config config = ConfigFactory.load("application_receiver");
+	      ActorSystem system = ActorSystem.create("ClusterSystem", config);
 	      system.actorOf(Props.create(SimpleListener.class),
 	          "receiver");
 	  }
